d98ebc2cf7e0c7c00fd8ad1f1d0b4220342880be
David W <svgPhoenix@proton.me>
2023-07-07 21:31:04 -0500

    refactor & beautify printing

--- src/contracts.ts
+++ src/contracts.ts
@@ -2,39 +2,63 @@ import { findServers } from "./phoenixLib";
 import * as solvers from "./contractSolvers";
 import { NS } from "@ns";
 
-export function main(ns:NS) {
+export function main(ns: NS) {
 	ns.disableLog("scan");
+	ns.disableLog("codingcontract.attempt");
 	ns.clearLog();
 	ns.tail();
-	let hosts = findServers(ns, ["home"], [], false);
+	let hosts = findServers(ns, ["home"], [], false),
+		rewards: string[] = [],
+		allContracts: ContractDirectory = {},
+		contractType: string,
+		data: any;
 	for (let host of hosts) {
 		let contracts = ns.ls(host, ".cct");
-		if (contracts.length > 0) { ns.print("\n" + host + ":\n" + contracts.toString() + "\n"); }
+		allContracts[host] = [];
 		for (let contract of contracts) {
-			let contractType = ns.codingcontract.getContractType(contract, host);
-			let data = ns.codingcontract.getData(contract, host);
+			contractType = ns.codingcontract.getContractType(contract, host),
+				data = ns.codingcontract.getData(contract, host);
 			switch (contractType) {
 				case ("Compression II: LZ Decompression"): {
-					let ans = solvers.compressionII(data);
-					ns.print(ns.codingcontract.attempt(ans, contract, host));
+					handleAttempt(solvers.compressionII, data, contract, host);
 				}
 				case ("Algorithmic Stock Trader I"): {
-					let ans = solvers.algorithmicStocksI(data);
-					ns.print(ns.codingcontract.attempt(ans, contract, host));
+					handleAttempt(solvers.algorithmicStocksI, data, contract, host);
 				}
 				case ("Encryption I: Caesar Cipher"): {
-					let ans = solvers.encryptionI(data);
-					ns.print(ns.codingcontract.attempt(ans, contract, host));
+					handleAttempt(solvers.encryptionI, data, contract, host);
 				}
 				case ("Compression I: RLE Compression"): {
-					let ans = solvers.compressionI(data);
-					ns.print(ns.codingcontract.attempt(ans, contract, host));
+					handleAttempt(solvers.compressionI, data, contract, host);
 				}
 				default: {
-					ns.print("unsolved contract type: " + contractType);
+					allContracts[host].push("\n" + contract + ": " + contractType);
 				}
 			}
 		}
 	}
+	//TODO finish the end-of-execution report
+	for (let server in allContracts) {
+		if (allContracts[server].length > 0) {
+			ns.print("\n" + server + ": " + allContracts[server]);
+		}
+	}
+	if (rewards.length > 0) {
+		ns.print("\nReward summary:\n" + rewards.join("\n"));
+	}
 
+	function handleAttempt(solver: Function, data: string, contract: string, host: string) {
+		ns.print("attempting " + contractType + " with data: " + data);
+		const ans = solver(data);
+		const reward = ns.codingcontract.attempt(ans, contract, host);
+		if (reward) {
+			rewards.push(reward);
+		} else {
+			ns.alert("Failed to solve " + contract + " on " + host + " with answer: " + ans);
+		}
+	}
+}
+
+interface ContractDirectory {
+	[index: string]: string[];
 }
