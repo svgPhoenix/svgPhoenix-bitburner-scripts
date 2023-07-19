e369932ddcb9340fe50444e0e43db8c601e3482a
David W <svgPhoenix@proton.me>
2023-07-18 23:25:22 -0500

    rm saveBonusFarmer; integrated with installAugs

--- src/saveBonusFarmer.ts
+++
@@ -1,8 +0,0 @@
-//IDEA wait for devs to add onClick() hook to toast messages
-
-import { NS } from '@ns';
-
-export async function main(ns: NS) {
-  ns.singularity.exportGameBonus();
-  ns.singularity.exportGame();
-}
