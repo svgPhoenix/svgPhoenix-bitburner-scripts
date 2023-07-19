eba7ff9a6f3f27fa557156137ecd85bfd02b312c
David W <svgPhoenix@proton.me>
2023-07-18 23:51:51 -0500

    IDEA added to installAugs

--- src/installAugs.ts
+++ src/installAugs.ts
@@ -1,6 +1,7 @@
 import { NS } from '@ns';
 
 export async function main(ns: NS) {
+  // IDEA upgrade home server and install Neuroflux Governer first
   if (ns.singularity.exportGameBonus()) ns.singularity.exportGame();
   ns.singularity.installAugmentations('startup.js');
 }
