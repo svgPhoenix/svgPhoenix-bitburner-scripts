5ab3d06eb0a1dc4bc74cbc84436daa41cb8ae11b
David W <svgPhoenix@proton.me>
2023-07-19 23:28:58 -0500

    add algorithms course to startup.ts

--- src/startup.ts
+++ src/startup.ts
@@ -6,10 +6,12 @@ export async function main(ns: NS) {
   // make or buy hacking programs
   // manage factions
 
-  const toKill = ['weaken.js', 'grow.js', 'hack.js', 'botnet.js'],
-    player = ns.getPlayer();
+  const toKill = ['weaken.js', 'grow.js', 'hack.js', 'botnet.js'];
+  let player = ns.getPlayer();
   multiScriptKill(ns, toKill, 'home');
   ns.exec('botnet.js', 'home', 1, '--ui');
   ns.exec('contractServerPurchaseWorker.js', 'home');
+  ns.singularity.universityCourse('Rothman University', 'Algorithms', false);
+  while (player.skills.hacking < 50) await ns.sleep(1000);
   if (player.skills.strength < 10) ns.exec('autoCrime.js', 'home');
 }
