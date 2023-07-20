98e77635d6fd0be6234ed2748f8cf629a4df9d18
David W <svgPhoenix@proton.me>
2023-07-19 21:25:47 -0500

    good idea doesn't work; keeping it in comments

--- src/autoCrime.ts
+++ src/autoCrime.ts
@@ -1,4 +1,4 @@
-import { NS } from '@ns';
+import { CrimeType, NS } from '@ns';
 
 export async function main(ns: NS) {
   while (ns.singularity.getCrimeChance('Mug') < 0.9) {
@@ -12,6 +12,31 @@ export async function main(ns: NS) {
     await ns.asleep(1000);
   }
   ns.singularity.commitCrime('Mug', false);
+  // const crimes = [
+  //   'Shoplift',
+  //   'Rob Store',
+  //   'Mug',
+  //   'Larceny',
+  //   'Deal Drugs',
+  //   'Bond Forgery',
+  //   'Traffick Arms',
+  //   'Homicide',
+  //   'Grand Theft Auto',
+  //   'Kidnap',
+  //   'Assassination',
+  //   'Heist'
+  // ];
+  // let bestReturn = 0,
+  //   bestCrime = '';
+  // for (const crime of crimes) {
+  //   const crimeStats = ns.singularity.getCrimeStats(crime as CrimeType),
+  //     currentReturn = (crimeStats.money / crimeStats.time) * ns.singularity.getCrimeChance(crime as CrimeType);
+  //   if (bestReturn < currentReturn) {
+  //     bestReturn = currentReturn;
+  //     bestCrime = crime;
+  //   }
+  // }
+  // ns.singularity.commitCrime(bestCrime as CrimeType, false);
   // while (ns.singularity.getCrimeChance('Traffick Arms') < 0.6) {
   //   await ns.asleep(5000);
   // }
